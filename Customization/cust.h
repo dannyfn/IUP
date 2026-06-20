0997c9314d3a03d332a8d97feaa4dca9e37bdeb6
eric <eric@innosz.com>
2026-06-20 21:47:15 +0800

    move feature define to cust.h

--- Customization/cust.h
+++ Customization/cust.h
@@ -3,6 +3,20 @@
 
 #define PRODUCT_NAME_MAX_LEN 16
 #define RFC_NAME_MAX_LEN 16
+
+/* Boot mode constants */
+#define BOOT_MODE_NORMAL    0x55775577
+#define BOOT_MODE_FTM       0x57575757
+#define BOOT_MODE_POFFCHG   0x75757575
+
+/* Feature mask bit definitions */
+#define  FEATURE_ADD_LED 0x1
+#define  FEATURE_ADD_LCD 0x2
+#define  FEATURE_NO_BATT 0x4
+#define  FEATURE_ADD_ETHERNET 0x8
+#define  FEATURE_NO_POWER_KEY 0x10
+
+
 enum{
     BOARD_QC02,
     BOARD_QC024200,
--- Customization/devinfo.h
+++ Customization/devinfo.h
@@ -2,29 +2,13 @@
 #define _DEVINFO_H_
 
 #include <stdbool.h>
-
+#include "cust.h"
 #define DEVICE_MAGIC        "ANDROID-BOOT!"
 #define DEVICE_MAGIC_SIZE   13
 #define MAX_PANEL_ID_LEN    64
 #define MAX_VERSION_LEN     64
 
-/* Boot mode constants */
-#define BOOT_MODE_NORMAL    0x55775577
-#define BOOT_MODE_FTM       0x57575757
-#define BOOT_MODE_POFFCHG   0x75757575
 
-/* Feature mask bit definitions */
-#define  FEATURE_ADD_LED 0x1
-#define  FEATURE_ADD_LCD 0x2
-#define  FEATURE_NO_BATT 0x4
-#define  FEATURE_ADD_ETHERNET 0x8
-#define  FEATURE_NO_POWER_KEY 0x10
-/* Known board names */
-#define BOARD_QC02          "QC02"
-#define BOARD_MF900         "MF900"
-#define BOARD_QC03          "QC03"
-#define BOARD_CP103 "CP103"
-#define BOARD_CP105 "CP105"
 
 struct device_info
 {
